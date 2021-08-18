4880f8989b752a7275079d46f7cfe501ec79b2b2
szwldev <14846869@qq.com>
2021-08-18 13:20:54 +0800

    testjs

---
+++ test.js
@@ -0,0 +1,14 @@
+var playerName = 'Adamin Music Player', autoPlayer = 1, randomPlayer = 0, defaultVolume = 80, showLrc = 1, greeting = 'Welcome',
+    showGreeting = 1, defaultAlbum = 1, siteName = '李大鹏', background = 1, playerWidth = 310, coverWidth = 110,
+    showNotes = 1, autoPopupPlayer = 3;
+var songSheetList = [{
+    "songSheetName": "recommend",
+    "author": "adamin",
+    "songIds": ["502678329","109734","1441860073","1674192"],
+    "songNames": ["Yasuo","凤凰花开的路口","Morsmordre","More Than I Can Say"],
+    "songTypes": ["wy","wy", "wy","wy"],
+    "albumNames": ["Yasuo","林志炫","Crazy Donkey","Leo Sayer" ],
+    "artistNames": ["Yasuo","林志炫","Crazy Donkey","Leo Sayer" ],
+    "albumCovers": ["https://p1.music.126.net/xOghBESBon_7xvF37PQEkQ==/109951165562353576.jpg?param=200y200","http://p1.music.126.net/0iKLtwsIwOMksbNkZUJLzg==/16671894812505379.jpg?param=130y130","http://p1.music.126.net/hZZF9j6Rd_Te3CXYqg5GIg==/109951165772600862.jpg?param=130y130"
+    ,"http://p4.music.126.net/xjoGtl3hxnTQdldOsibxRA==/18593841139166266.jpg?param=200y200"]
+},  ]
